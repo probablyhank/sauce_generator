6c4ae226f0f2cf64dce209b7086fc4486d5381b5
evwaters <evwaters@WizRig>
2022-12-20 16:59:30 -0600

    init of openai & ability to pass prompts via path params @ /prompt endpoint

--- sauce_generator/big_brain.py
+++ sauce_generator/big_brain.py
@@ -1,1 +1,22 @@
 # TODO: Add openAI code
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# replace this value with your personal openai api key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def custom_prompt(cPrompt):
+    cResponse = openai.Completion.create(
+        model="text-ada-001",
+        prompt=cPrompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.2,
+        presence_penalty=0,
+    )
+    return cResponse
--- sauce_generator/main.py
+++ sauce_generator/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from .big_brain import custom_prompt
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "message": custom_prompt(
+            # enter your prompt
+            "Write a sentence about a grumpy programmer named Henry."
+        )
+    }
+
+
+@app.get("/prompt/{prompt}")
+async def create_prompt(prompt):
+    return {"output": custom_prompt(prompt)}
