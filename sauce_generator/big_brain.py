ecfb7b1ce7edc0cafb8c37b60a1be464520e0b7b
probablyhank <108912951+probablyhank@users.noreply.github.com>
2022-12-20 20:43:20 -0600

    Open AI Stuffs (#1)
    * init of openai & ability to pass prompts via path params @ /prompt endpoint
    
    * enable model swapping via query params & more housekeeping
    
    Author: evwaters <evwaters@WizRig>

--- sauce_generator/big_brain.py
+++ sauce_generator/big_brain.py
@@ -1,1 +1,20 @@
-# TODO: Add openAI code
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def custom_prompt(c_prompt, c_model):
+    c_response = openai.Completion.create(
+        model=c_model,
+        prompt=c_prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=2.0,
+        presence_penalty=1.0,
+    )
+    return c_response
--- sauce_generator/main.py
+++ sauce_generator/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+# TODO implement validation for model & throw exceptions for bad inputs
+from fastapi import FastAPI, Query
+from .big_brain import custom_prompt
 
 app = FastAPI()
 
 
+def create_model(name):
+    return f"text-{name}-001"
+
+
+# more expensive, far richer results
+davinci_model = "text-davinci-003"
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to openAI via URL."}
+
+
+@app.get("/prompt/{prompt}")
+async def create_prompt(
+    prompt: str,
+    model=Query(
+        default=create_model("ada"),
+        description="change the middle value to 'ada', 'babbage', or 'curie'",
+    ),
+):
+    return {"output": custom_prompt(prompt, model)}
