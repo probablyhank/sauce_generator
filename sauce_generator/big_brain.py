f3275535958d0920f6f97c2a3ecf0afaa170f39e
evwaters <evwaters@WizRig>
2022-12-20 20:36:53 -0600

    enable model swapping via query params & more housekeeping

--- sauce_generator/big_brain.py
+++ sauce_generator/big_brain.py
@@ -6,24 +6,15 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# cheapest, simplest results
-ada_model = "text-ada-001"
-# medium cost & richness
-babbage_model = "text-babbage-001"
-# cheaper than davinci but still quite expensive
-curie_model = "text-curie-001"
-# more expensive, far richer results
-davinci_model = "text-davinci-003"
 
-
-def custom_prompt(cPrompt):
-    cResponse = openai.Completion.create(
-        model=ada_model,
-        prompt=cPrompt,
+def custom_prompt(c_prompt, c_model):
+    c_response = openai.Completion.create(
+        model=c_model,
+        prompt=c_prompt,
         temperature=0,
         max_tokens=100,
         top_p=1,
-        frequency_penalty=0.2,
-        presence_penalty=0,
+        frequency_penalty=2.0,
+        presence_penalty=1.0,
     )
-    return cResponse
+    return c_response
